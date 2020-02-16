95a6db7db933c2f87929df26fb5171c253de9738
stsang123412 <stsang@hawk.iit.edu>
2020-02-16 07:34:58 -0600

    adding mortgage files to 419 repo

---
+++ Mortgage-proj2-stsang/Mortgage-proj2-stsang/MortgageClassCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mortgage_proj2_stsang
+{
+	public class MortgageCalculator
+	{
+		//function 
+		//calculates monthly payment
+		//takes in our 3 variables: loanAmount | interestAmount | durationTime
+		//							principal  | rate			| years
+
+		public static double CalculateMonthlyPaymentFunction(string stringprincipal, string stringYears, string stringRate)
+		{
+			double monthly;
+
+			double principal = Convert.ToDouble(stringprincipal);
+			double years = Convert.ToDouble(stringYears);
+			double rate = Convert.ToDouble(stringRate);
+
+			double top = principal * rate / 1200.00;
+			double bottom = 1 - Math.Pow(1.0 + rate / 1200.00, -12.0 * years);
+			monthly = top / bottom;
+
+			return monthly;
+		}
+	}
+}
+
+
---
+++ Mortgage-proj2-stsang/Mortgage-proj2-stsang/Pages/Index.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mortgage_proj2_stsang.Pages
+{
+    public class IndexModel : PageModel
+    {
+            public string Msg
+            {
+                get;            //public; anyone can get
+                private set;    //private; not anyone can change the value
+            }
+            [Required]
+            [BindProperty]
+            public string principalString
+            // this variable is linked to index.cshtml
+            // <input type="text" asp-for="loanAmount" value=""/>
+            {
+                get;            //public; anyone can get
+                set;            //no private; must be set by the 
+            }
+            public string yearString
+            // this variable is linked to index.cshtml
+            // <input type="text" asp-for="durationTime" value=""/>
+            {
+                get;            //public; anyone can get
+                set;            //no private; must be set by the 
+            }
+            public string rateString
+            // this variable is linked to index.cshtml
+            // <input type="text" asp-for="interestAmount" value=""/>
+            {
+                get;            //public; anyone can get
+                set;            //no private; must be set by the 
+            }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Msg = "This small application will calculate your loan information based on the information you provide";
+        }
+        public void OnPost()
+        {
+            string principal = Request.Form["principalString"];
+            string years = Request.Form["yearString"];
+            string rate = Request.Form["rateString"];
+
+            double showMonthlyPayment = MortgageCalculator.CalculateMonthlyPaymentFunction(principal, years, rate);
+
+            Msg = $"${showMonthlyPayment} is your monthly payment over the course of {years} years at a rate of {rate}% interest.";
+        }
+    }
+}
